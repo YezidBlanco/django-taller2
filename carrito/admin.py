689bf8242681253b06d0898b1a104b9bee3f81ed
YezidBlanco <yezidblanco@gmail.com>
2024-07-24 23:48:29 -0500

    Agregando modelos

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from carrito.models import Producto, ItemCarrito, Carrito
 
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["id","nombre", "descripcion", "precio", "inventario"]
+    search_fields = ["nombre", "descripcion", "precio", "inventario"]
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(ItemCarrito)
+admin.site.register(Carrito)
--- carrito/models.py
+++ carrito/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(unique=True,max_length=100)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    inventario = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.nombre} {self.descripcion} {self.precio} {self.inventario}"
+
+class Carrito(models.Model):
+    name = models.CharField(max_length=128)
+    productos = models.ManyToManyField(Producto, through="ItemCarrito")
+
+    def __str__(self):
+        return self.name
+
+class ItemCarrito(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+
+
+    
